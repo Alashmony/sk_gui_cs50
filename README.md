dbce706bcd0fee829c32752247b77ba57a85933b
alashmony <a.alashmony@outlook.com>
2024-04-06 06:34:22 +0200

    Finished data upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# sk_gui_cs50
-SkiKit-Learn GUI as the final project for CS50
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, session, render_template, redirect, request
 from flask_session import Session
+import pandas as pd
 
 app = Flask(__name__)
+accepted_ext = ["csv", "xls", "xlsx"]
+
+df = pd.DataFrame()
 
 # Stop permenant session
 app.config["SESSION_PERMANENT"] = False
@@ -17,5 +21,27 @@ def home():
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
-    if request.method == "GET":
-        return render_template("upload.html")
+    if request.method == "POST":
+        # Let's get the extension
+        file = request.files["file"]
+        file.save(file.filename)
+        filename_inverted = file.filename[::-1]
+        ext = filename_inverted[: filename_inverted.find(".")][::-1]
+        # if the provided extension is not in the accepted ones
+        if ext not in accepted_ext:
+            error = "Unacceptable format!"
+            return render_template("upload.html", exts=accepted_ext, error=error)
+
+        if ext == "csv":
+            df = pd.read_csv(file.filename, low_memory=False)
+        if ext == "xlsx" or ext == "xls":
+            df = pd.read_excel(file.filename)
+
+        columns = list(df.columns)
+        rows = df.head(10).to_dict(orient="records")
+        print(rows)
+        return render_template(
+            "upload.html", file=file.filename, columns=columns, rows=rows
+        )
+
+    return render_template("upload.html", exts=accepted_ext)
