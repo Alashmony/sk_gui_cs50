73033f19b9ed994a27d9d85fc7a1237f870cc78c
alashmony <a.alashmony@outlook.com>
2024-04-06 04:54:25 +0200

    Finished home page

--- app.py
+++ app.py
@@ -13,3 +13,8 @@ Session(app)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
