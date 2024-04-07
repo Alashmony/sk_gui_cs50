3136c224a7f1b84beaa62cffdfd2f2ed61b7eeec
alashmony <a.alashmony@outlook.com>
2024-04-07 19:16:21 +0200

    Finished data manipulation and renamed project to Pandas Playground

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template, redirect, request
+from flask import Flask, session, render_template, redirect, request, url_for
 from flask_session import Session
 from werkzeug.utils import secure_filename
 import pandas as pd
@@ -36,23 +36,117 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/remove_steps", methods=["POST"])
+def remove_steps():
+    # Find step to remove
+    step = request.form.get("step_to_remove")
+    step_name = list(session["steps"][-1].keys())[0]
+    step_n = int(step_name[step_name.find("_") + 1 :])
+    steps = []
+    dfs = {}
+    # To reset all the proccess
+    if step == "":
+        session["steps"] = []
+        session["df"] = {"v0": session["df"]["v0"]}
+    # If the step # provided is more than the latest step
+    elif int(step) > step_n:
+        return redirect(url_for("manipulate", error_msg="This step is not found"))
+    else:
+        # try to perform calculations to the main df without this step
+        try:
+            # reapply all steps
+            last_step = 0
+            for step_dict in session["steps"]:
+                step_name = list(step_dict.keys())[0]
+                step_code = step_dict[step_name]
+                print(step_name, step_code)
+                # Get current step #
+                step_number = int(step_name[step_name.find("_") + 1 :])
+                # if this is the removed step
+                if step_number == int(step):
+                    continue
+                # reapply step and store the df
+                else:
+                    dfs["v0"] = session["df"]["v0"]
+                    df = dfs["v" + str(last_step)]
+                    dfs["v" + str(step_number)] = eval(step_code)
+                    steps.append({step_name: step_code})
+                    last_step = step_number
+            session["steps"] = steps
+            session["df"] = dfs
+        except Exception as e:
+            return redirect(url_for("manipulate", error_msg=str(e)))
+    return redirect("/manipulate")
+
+
 @app.route("/manipulate", methods=["GET", "POST"])
 def manipulate():
-    if request.method == "GET":
-        df = session["df"]["v0"]
-        rows = df.head(10).to_dict(orient="records")
-        columns = list(session["df"]["v0"].columns)
-        file = session["filename"]
+    # Create steps to store the steps
+    if "steps" not in session or len(session["steps"]) == 0:
+        session["steps"] = []
+        step_n = 1
+    else:
+        step_name = list(session["steps"][-1].keys())[0]
+        step_n = int(step_name[step_name.find("_") + 1 :]) + 1
 
-        methods_ = get_methods(df)
-        return render_template(
-            "manipulate.html",
-            file=file,
-            rows=rows,
-            columns=columns,
-            methods_=methods_.keys(),
-            descriptions=methods_,
-        )
+    if request.method == "GET":
+        step_n -= 1
+
+    error_msg = request.args.get("error_msg")
+
+    # If a new step was sent
+    if request.method == "POST":
+        func = request.form.get("function")
+        args = request.form.get("code")
+        close = request.form.get("close")
+        # print(func, args, close)
+        step = func + args + close
+
+        # Find if inplace is used to remove it
+        try:
+            step = step.replace(" ,", ",").replace("  ", " ").replace(", ", ",")
+            step = step.replace(
+                step[step.find("inplace") : step.find(",", step.find("inplace")) + 1],
+                "",
+            )
+            step = step.replace(
+                step[step.find("inplace") : step.find(")", step.find("inplace"))], ""
+            )
+            step = step.replace(",)", ")")
+        except:
+            pass
+
+        # Check if it is not a duplicated step  or a refresh
+        if step_n == 1 or step != list(session["steps"][-1].values())[0]:
+            df = session["df"]["v" + str(step_n - 1)]
+            session["steps"].append({"step_" + str(step_n): step})
+            session["df"]["v" + str(step_n)] = eval(step)
+
+    file = session["filename"]
+
+    # If the tranformations leaded to a new DF, not a different type
+    try:
+        columns = list(session["df"]["v" + str(step_n)].columns)
+        rows = session["df"]["v" + str(step_n)].head(10).to_dict(orient="records")
+        methods_ = get_methods(session["df"]["v" + str(step_n)])
+    except:
+        columns = list(session["df"]["v0"].columns)
+        rows = session["df"]["v0"].head(10).to_dict(orient="records")
+        methods_ = get_methods(session["df"]["v0"])
+    # print(session["steps"])
+    params = {
+        "file": file,
+        "rows": rows,
+        "columns": columns,
+        "methods_": methods_.keys(),
+        "descriptions": methods_,
+        "columnsize": len(columns),
+    }
+    if len(session["steps"]) > 0:
+        params["applied_steps"] = session["steps"]
+    if error_msg != None:
+        params["error_msg"] = error_msg
+    return render_template("manipulate.html", **params)
 
 
 @app.route("/explore")
